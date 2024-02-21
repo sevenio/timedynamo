01c4aeea42571f13025192ea4ed582d72529e867
gangadhar <gangadhar.vasa@tvisha.in>
2024-02-21 15:28:44 +0530

    refactor customviews to kotlin

--- app/src/main/java/com/tvisha/trooptime/activity/activity/dialog/CustomProgressBar.kt
+++ app/src/main/java/com/tvisha/trooptime/activity/activity/dialog/CustomProgressBar.kt
@@ -1,61 +1,39 @@
-package com.tvisha.trooptime.activity.activity.dialog;
+package com.tvisha.trooptime.activity.activity.dialog
 
-import android.app.Dialog;
-import android.content.Context;
-import android.os.Bundle;
-import android.view.Window;
-import android.widget.ImageView;
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.ldoublem.loadingviewlib.LVCircularCD
+import com.ldoublem.loadingviewlib.view.LVCircularRing
+import com.tvisha.trooptime.R
+import com.tvisha.trooptime.databinding.CustomProgressBarBinding
 
-import com.bumptech.glide.Glide;
-import com.ldoublem.loadingviewlib.LVCircularCD;
-import com.ldoublem.loadingviewlib.view.LVCircularRing;
-import com.tvisha.trooptime.R;
+class CustomProgressBar @JvmOverloads constructor(context: Context, style: Int = R.style.common_dialog_progrss) :
+    Dialog(context, style) {
 
-
-public class CustomProgressBar extends Dialog {
-
-    boolean isCancel;
-    ImageView imageView;
-    Context context;
-    LVCircularCD lvCircularCD;
-    LVCircularRing lvCircularRing;
-
-    public CustomProgressBar(Context context) {
-        super(context, R.style.common_dialog_progrss);
-        this.context = context;
+    private val binding by lazy {
+        CustomProgressBarBinding.inflate(layoutInflater)
     }
 
-    public CustomProgressBar(Context context, boolean isCancel) {
-        super(context, R.style.common_dialog_progrss);
-        this.isCancel = isCancel;
-        this.context = context;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.custom_progress_bar);
-        imageView = findViewById(R.id.image);
-
-
-        Glide.with(context)
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        binding.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setContentView(root)
+            Glide.with(context)
                 .load(R.drawable.progressbar_rotate)
-                .into(imageView);
+                .into(binding.image)
+            val colorAccent = ContextCompat.getColor(context, R.color.colorAccent)
 
-        int colorAccent = context.getResources().getColor(R.color.colorAccent);
-        int colorPrimaryDark = context.getResources().getColor(R.color.colorPrimaryDark);
-        int colorPrimary = context.getResources().getColor(R.color.colorPrimary);
-
-        lvCircularCD = findViewById(R.id.lvCircularCD);
-        lvCircularCD.setViewColor(colorAccent);
-        lvCircularCD.startAnim();
-
-        lvCircularRing = findViewById(R.id.lvCircularRing);
-        lvCircularRing.setBarColor(colorAccent);
-        lvCircularRing.startAnim();
-
-        setCancelable(isCancel);
+            lvCircularCD.setViewColor(colorAccent)
+            lvCircularCD.startAnim()
+            lvCircularRing.setBarColor(colorAccent)
+            lvCircularRing.startAnim()
+        }
+        setCancelable(false)
     }
-
-
 }
